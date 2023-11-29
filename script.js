49c62133f551d5c53714966b2bf6037fc0a9c9f5
lin100123 <lin87100123@gmail.com>
2023-11-29 22:07:00 +0800

    更新

---
+++ script.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var topBtn = document.getElementById("topBtn");
+
+   
+    window.onscroll = function() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            topBtn.style.display = "block";
+        } else {
+            topBtn.style.display = "none";
+        }
+    };
+    topBtn.addEventListener("click", function() {
+        document.body.scrollTop = 0; 
+        document.documentElement.scrollTop = 0; 
+    });
+});
+
+
